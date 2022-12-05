de7aef48910ba81db87a5ae5d30c0eef7c5f57b0
codick <114742863+codick@users.noreply.github.com>
2022-12-05 03:22:59 +0300

    Add files via upload

--- glybokie_routes/views.py
+++ glybokie_routes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, JsonResponse
 
 
 def posts(request):
@@ -31,7 +31,31 @@ def login_password(request):
     return HttpResponse(f'{login}, {password}')
 
 def about(request):
-    return HttpResponseRedirect("/posts")
+    return HttpResponseRedirect("<p>Об этом</p>")
 
-def details(request):
-    return HttpResponsePermanentRedirect("/")
+def contacts(request):
+    return HttpResponsePermanentRedirect("<p>Контакты</p>")
+
+def error_404(request):
+    return HttpResponseNotFound('Загрузка страницы была завершена ошибкой')
+
+def access(request, login, password):
+    login = request.GET.get('login', 'admin')
+    password = request.GET.get('password', 'admin')
+    if login == 'admin' and password == 'admin':
+        return HttpResponse('<p>Все норм</p>')
+    else:
+        return HttpResponse('<p>Неверные логин и пароль</p>')
+
+def json(request):
+    return JsonResponse({"name": "Sasha", "age": 17})
+
+def set(request):
+    username = request.GET.get('username', 'Sasha')
+    response = HttpResponse(f"Hello {username}")
+    response.set_cookie('username', username)
+    return response
+
+def get(request):
+    username = request.COOKIES['username']
+    return HttpResponse(f"Hello {username}")
--- routes/urls.py
+++ routes/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('posts/', include(posts)),
     path('login_password', views.login_password),
     path('about', views.about),
-    path('details', views.details)
+    path('contacts', views.contacts),
+    path('error_404', views.error_404),
+    path('access/', views.access),
+    path('json', views.json),
+    path('set', views.set),
+    path('get', views.get)
 ]
